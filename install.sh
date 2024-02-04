a4eb15378d49b6a1a9dbbabd50722359331d744e
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-02-04 16:05:18 -0300

    FEAT: Adding the Installation ShellScript

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Run:
+# chmod +x install.sh
+# sudo ./install.sh
+
+# JavaScript (Node.js and NPM):
+sudo apt-get update
+sudo apt-get install nodejs npm -y
+
+# NPM Packages Globally:
+npm install -g jshint
+npm install -g nodemon
